c7b5afcc0b20eeb8ce16800818011d6d9df4e156
Natasha <3686091@gmail.com>
2024-02-26 15:36:39 +0300

    фикс, апи перестало отдавать по несколько элементов за раз

--- app/hooks/useFetchPictures.tsx
+++ app/hooks/useFetchPictures.tsx
@@ -6,16 +6,19 @@ const useFetchPictures = () => {
   const [pictures, setPictures] = useState([]);
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
+  const [count, setCount] = useState(20);
 
   useEffect(() => {
+    if (count === 0) return;
     fetch(
-      `https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=${20}`
+      `https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1`
     )
       .then((response) => response.text())
       .then((result) => dispatch(setPicturesData(JSON.parse(result))))
       .then(() => setIsLoading(false))
       .catch((error) => console.log("error", error));
-  }, []);
+    setCount((prev) => prev - 1);
+  }, [count]);
 
   return { pictures, setPictures, isLoading };
 };
